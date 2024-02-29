4de81c546e36c5c32d530d84551db4451438e901
Chinmay-A <chinmayaons1@gmail.com>
2024-02-29 21:28:13 +0530

    first commit- get connection to mysql server

---
+++ data-platform/sql_connector.py
@@ -0,0 +1,12 @@
+import os
+import dotenv
+import mysql.connector as mysql
+
+dotenv.load_dotenv()
+
+SQLUSER=os.getenv('MYSQL_USER')
+SQLPASS=os.getenv('MYSQL_PASS')
+
+def connectSQL():
+    connection=mysql.connect(username=SQLUSER,password=SQLPASS,host='localhost',database='cryptos')
+    return connection
