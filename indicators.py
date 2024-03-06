91a3afee6d42064e71b6b4b78e5156c137b9e22f
Chinmay-A <chinmayaons1@gmail.com>
2024-03-06 21:11:49 +0530

    added ema and bollinger bands

--- indicators.py
+++ indicators.py
@@ -1,11 +1,27 @@
-class Indicators:
-
-    def __init__(self):
-        self.name="Indicators"
+def sma(window, series):
     
-    def sma(self,window, series):
-        
-        if(window>len(series)):
-            return -1
+    if(window>len(series)):
+        raise Exception("Window is longer than the available data")
 
-        return (sum(series[-window:])/window)
+    return (sum(series[-window:])/window)
+
+def ema(window, k, series):
+
+    if(window>len(series)):
+        raise Exception("Window is longer than the available data")
+    
+    if(window==len(series)):
+        return sma(window,series)
+    
+
+    x=ema(window,k,series[-1])
+    return k*(series[-1]-x)+x
+
+def bollinger_bands(window,series,devs):
+
+    import statistics
+
+    middle=sma(window,series)
+    stdev=statistics.stdev(series[-window:])
+    
+    return middle-devs*stdev,middle,middle+stdev
